77b19f43b590f6c2b3da366cd86e55863ff5be31
LLACHICA <llachica.578@gmail.com>
2023-03-20 18:33:09 -0400

    initial commit

---
+++ var.tf
@@ -0,0 +1,10 @@
+variable "location" {
+    type = string
+    description = "Region"
+    default = "Canada Central"
+}
+variable "resource_group_name" {
+  type    = string
+  description = "Resource Group name"
+  default = "Ansible-Project"
+}
